93566adc8dc00929a93a618daa4d427055747fd6
MoXiaobai1412 <162461588+MoXiaobai1412@users.noreply.github.com>
2025-08-06 16:06:54 +0800

    2
    aaa

---
+++ app.js
@@ -0,0 +1,44 @@
+// app.js
+App({
+  globalData: {
+    userInfo: null
+  },
+  onLaunch() {
+    wx.cloud.init({  
+      env: 'scuttutor-guangdong1-1bf905cb6c6',  
+      traceUser: true,
+    })
+    wx.cloud.callFunction({
+      name: 'getConfig',
+      complete: res => {
+        wx.setStorageSync('openId', res.result.openid)
+        wx.setStorageSync('appId', res.result.appid)
+      }
+    })
+    const db = wx.cloud.database()
+    
+
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  onHide() {
+    const db = wx.cloud.database()
+    const openId = wx.getStorageSync('openId')
+    const favorList = wx.getStorageSync('settings').favorList
+    db.collection('settings').where({
+      _openid:openId
+    }).update({
+      data:{
+        favorList:favorList
+      }
+    })
+  },
+})
